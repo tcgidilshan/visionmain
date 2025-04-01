08d5fcd045331c1b9c009fd0db897ac5e0b2e626
lahirusb97 <lahirushirandev@gmail.com>
2025-04-01 20:35:13 +0530

    pagination-invoice

--- api/views/invoice_search_views.py
+++ api/views/invoice_search_views.py
@@ -3,10 +3,15 @@ from rest_framework.response import Response
 from rest_framework import permissions, status
 from ..services.Invoice_service import InvoiceService
 from ..serializers import InvoiceSerializer
+from rest_framework.pagination import PageNumberPagination
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10  # Default page size
+    page_size_query_param = 'page_size'  # Allow client to override
+    max_page_size = 100  # Maximum limit
 
 class FactoryInvoiceSearchView(APIView):
     permission_classes = [permissions.IsAuthenticated]
-
+    pagination_class = StandardResultsSetPagination
     def get(self, request):
         invoice_number = request.query_params.get('invoice_number')
         mobile = request.query_params.get('mobile')
@@ -24,6 +29,11 @@ class FactoryInvoiceSearchView(APIView):
             mobile=mobile,
             nic=nic
         )
-
+        paginator = self.pagination_class()
+        page = paginator.paginate_queryset(invoices, request)
+        if page is not None:
+            serializer = InvoiceSerializer(page, many=True)
+            return paginator.get_paginated_response(serializer.data)
+        
         serializer = InvoiceSerializer(invoices, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
